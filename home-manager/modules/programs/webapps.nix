7d9a46dc241ea9b0fc5ec712e5f81b3b865aa890
mostlykiguess <bruvistrue93@gmail.com>
2026-06-24 23:31:56 +0530

    SVG Icon fix for WebApps

--- home-manager/modules/programs/webapps.nix
+++ home-manager/modules/programs/webapps.nix
@@ -1,21 +1,23 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  papirusApps = "${pkgs.papirus-icon-theme}/share/icons/Papirus/128x128/apps";
+in
 {
-  # Web apps launched in Helium's app mode (chromeless standalone windows).
   xdg.desktopEntries = {
     whatsapp = {
       name = "WhatsApp";
       exec = "helium --app=https://web.whatsapp.com";
-      icon = "whatsapp";
+      icon = "${papirusApps}/whatsapp.svg";
       categories = [ "Network" "InstantMessaging" ];
-      settings.StartupWMClass = "web.whatsapp.com";
+      settings.StartupWMClass = "chrome-web.whatsapp.com__-Default";
     };
 
     rrc-zulip = {
       name = "RRC Zulip";
       exec = "helium --app=https://rrc-iiith.zulipchat.com";
-      icon = "zulip";
+      icon = "${papirusApps}/zulip.svg";
       categories = [ "Network" "InstantMessaging" ];
-      settings.StartupWMClass = "rrc-iiith.zulipchat.com";
+      settings.StartupWMClass = "chrome-rrc-iiith.zulipchat.com__-Default";
     };
   };
 }
