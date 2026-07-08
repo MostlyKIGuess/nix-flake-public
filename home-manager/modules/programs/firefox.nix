ebc8adbcb9bd08eb176e26bd3ed9d0797e15ae0a
mostlykiguess <bruvistrue93@gmail.com>
2026-07-08 16:25:36 +0530

    fix: restore evince/loupe mime defaults over chromium

--- home-manager/modules/programs/firefox.nix
+++ home-manager/modules/programs/firefox.nix
@@ -32,7 +32,30 @@ in
     defaultApplications = {
       "x-scheme-handler/http" = "firefox.desktop";
       "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/chrome" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
+      "x-scheme-handler/mailto" = "firefox.desktop";
       "text/html" = "firefox.desktop";
+      "application/xhtml+xml" = "firefox.desktop";
+
+      "application/pdf" = "org.gnome.Evince.desktop";
+
+      "image/png" = "org.gnome.Loupe.desktop";
+      "image/jpeg" = "org.gnome.Loupe.desktop";
+      "image/gif" = "org.gnome.Loupe.desktop";
+      "image/webp" = "org.gnome.Loupe.desktop";
+      "image/svg+xml" = "org.gnome.Loupe.desktop";
+      "image/bmp" = "org.gnome.Loupe.desktop";
+      "image/tiff" = "org.gnome.Loupe.desktop";
+      "image/avif" = "org.gnome.Loupe.desktop";
+
+      "x-scheme-handler/discord" = "vesktop.desktop";
+      "x-scheme-handler/sgnl" = "signal.desktop";
+      "x-scheme-handler/signalcaptcha" = "signal.desktop";
+      "x-scheme-handler/slack" = "slack.desktop";
+      "x-scheme-handler/magnet" = "org.qbittorrent.qBittorrent.desktop";
+      "x-scheme-handler/claude-cli" = "claude-code-url-handler.desktop";
     };
   };
 }
